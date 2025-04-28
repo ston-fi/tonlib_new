b8df163a363f86ba5c5279eb23a61fc8a5f77986
Sild <silddev@icloud.com>
2025-04-28 14:07:57 +0200

    add Coins converters

--- ton_lib/src/types/tlb/tep_0074/jetton_transfer_msg.rs
+++ ton_lib/src/types/tlb/tep_0074/jetton_transfer_msg.rs
@@ -4,10 +4,9 @@ use crate::cell::ton_cell::{TonCell, TonCellRef};
 use crate::types::tlb::block_tlb::coins::Coins;
 use crate::types::tlb::block_tlb::msg_address::{MsgAddress, MsgAddressInt, MsgAddressNone};
 use crate::types::tlb::primitives::EitherRef;
-use num_bigint::BigUint;
 use ton_lib_macros::TLBDerive;
 
-#[derive(Clone, Debug, TLBDerive)]
+#[derive(Clone, Debug, TLBDerive, PartialEq)]
 #[tlb_derive(prefix = 0x0f8a7ea5, bits_len = 32)]
 pub struct JettonTransferMsg {
     pub query_id: u64, // arbitrary number to identify the transfer
@@ -20,15 +19,55 @@ pub struct JettonTransferMsg {
 }
 
 impl JettonTransferMsg {
-    pub fn new<A: Into<BigUint>>(dst: MsgAddressInt, amount: A) -> Self {
+    pub fn new<T: Into<Coins>>(dst: MsgAddressInt, amount: T) -> Self {
         JettonTransferMsg {
             query_id: 0,
-            amount: Coins::new(amount),
+            amount: amount.into(),
             dst: dst.into(),
             response_dst: MsgAddressNone {}.into(),
             custom_payload: None,
             forward_ton_amount: Coins::zero(),
             forward_payload: EitherRef::new(TonCell::EMPTY),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cell::build_parse::builder::CellBuilder;
+    use crate::types::tlb::primitives::EitherRefLayout;
+    use crate::types::tlb::tlb_type::TLBType;
+    use crate::types::ton_address::TonAddress;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_jetton_transfer_msg() -> anyhow::Result<()> {
+        // int_msg from https://tonviewer.com/transaction/18679bed03915803746469e9fe498add0ffecd76ae3056bb9c3777c9f722becd
+        let msg_boc = "b5ee9c720101020100650001b40f8a7ea55ecf57d735066d2460246139ca800800f52547902494daa24c332ecb41067ee9b6bae7b244a68ce0c5007ddc22f4b01f001f5d9cc275e5514e8386836ef59caa82e043c006d404f512ab7ee893e38f5f8d8847868c0101000be8e8e46c0020";
+        let msg = JettonTransferMsg::from_boc_hex(msg_boc)?;
+
+        let mut pl_builder = CellBuilder::new();
+        pl_builder.write_bits([232, 232, 228, 108, 0, 0], 42)?;
+        let payload = pl_builder.build()?;
+
+        let exp_msg = JettonTransferMsg {
+            query_id: 6831775741563530532,
+            amount: Coins::from_str("2500000000000")?,
+            dst: TonAddress::from_str("0:7a92a3c8124a6d5126199765a0833f74db5d73d92253467062803eee117a580f")?
+                .to_msg_address_int()
+                .into(),
+            response_dst: TonAddress::from_str("0:7d767309d795453a0e1a0dbbd672aa0b810f001b5013d44aadfba24f8e3d7e36")?
+                .to_msg_address_int()
+                .into(),
+            custom_payload: None,
+            forward_ton_amount: Coins::new(600000000u128),
+            forward_payload: EitherRef {
+                value: payload,
+                layout: EitherRefLayout::ToRef,
+            },
+        };
+        assert_eq!(exp_msg, msg);
+        Ok(())
     }
 }
