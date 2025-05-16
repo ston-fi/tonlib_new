f5ce57b6f389b282392843e49b10a8ba1268e93e
Sild <silddev@icloud.com>
2025-05-16 19:33:41 +0200

    update emulator interface

--- ton_lib/src/emulators/tvm/c7_register.rs
+++ ton_lib/src/emulators/tvm/c7_register.rs
@@ -4,25 +4,50 @@ use crate::types::ton_address::TonAddress;
 use base64::engine::general_purpose::STANDARD;
 use base64::Engine;
 use std::ffi::CString;
+use std::ops::Deref;
+use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Clone, Debug)]
+pub struct EmulatorBCConfig(Arc<CString>);
+
+impl Deref for EmulatorBCConfig {
+    type Target = CString;
+
+    fn deref(&self) -> &Self::Target { &self.0 }
+}
+
+impl From<Arc<CString>> for EmulatorBCConfig {
+    fn from(config: Arc<CString>) -> Self { Self(config) }
+}
+
+impl EmulatorBCConfig {
+    pub fn from_boc(config_boc: &[u8]) -> Result<Self, TonlibError> { Self::from_boc_b64(&STANDARD.encode(config_boc)) }
+    pub fn from_boc_hex(config_boc_hex: &str) -> Result<Self, TonlibError> {
+        Self::from_boc_b64(&STANDARD.encode(hex::decode(config_boc_hex)?))
+    }
+    pub fn from_boc_b64(config_boc_b64: &str) -> Result<Self, TonlibError> {
+        Ok(Self(Arc::new(CString::new(config_boc_b64)?)))
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct TVMEmulatorC7 {
-    pub address: CString,
+    pub address: TonAddress,
     pub unix_time: u32,
     pub balance: u64,
     pub rand_seed: TonHash,
-    pub config: CString,
+    pub config: EmulatorBCConfig,
 }
 
 impl TVMEmulatorC7 {
-    pub fn new(address: TonAddress, config_boc: &[u8]) -> Result<Self, TonlibError> {
+    pub fn new(address: TonAddress, config: EmulatorBCConfig) -> Result<Self, TonlibError> {
         Ok(Self {
-            address: CString::new(address.to_hex().as_bytes())?,
+            address,
             unix_time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as u32,
             balance: 0,
             rand_seed: TonHash::ZERO,
-            config: CString::new(STANDARD.encode(&config_boc))?,
+            config,
         })
     }
 }
--- ton_lib/src/emulators/tvm/tvm_response.rs
+++ ton_lib/src/emulators/tvm/tvm_response.rs
@@ -6,6 +6,75 @@ use base64::Engine;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
+pub struct TVMRunMethodSuccess {
+    pub vm_exit_code: i32,
+    pub vm_log: Option<String>,
+    pub stack_boc_b64: String, // b64(String) <--> Vec<u8> <--> TVMStack
+    pub gas_used: i32,
+    pub raw_response: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub struct TVMRunMethodResponse {
+    pub success: bool,
+    pub vm_exit_code: Option<i32>,
+    pub vm_log: Option<String>,
+    pub stack: Option<String>,
+    pub gas_used: Option<String>,
+    pub missing_library: Option<String>,
+    pub error: Option<String>,
+    #[serde(skip)]
+    pub raw_response: String,
+}
+
+impl TVMRunMethodResponse {
+    pub fn from_json(json: String) -> Result<Self, TonlibError> {
+        let mut value: Self = serde_json::from_str(&json)?;
+        value.raw_response = json;
+        Ok(value)
+    }
+
+    pub fn into_success(self) -> Result<TVMRunMethodSuccess, TonlibError> {
+        if !self.success {
+            return Err(TonlibError::TVMRunMethodError {
+                vm_exit_code: self.vm_exit_code,
+                response_raw: self.raw_response,
+            });
+        }
+        let vm_exit_code = unwrap_opt(self.vm_exit_code, "vm_exit_code")?;
+        if vm_exit_code != 0 && vm_exit_code != 1 {
+            return Err(TonlibError::TVMRunMethodError {
+                vm_exit_code: self.vm_exit_code,
+                response_raw: self.raw_response,
+            });
+        }
+
+        let vm_log = self.vm_log;
+        let stack_boc_b64 = unwrap_opt(self.stack, "stack")?;
+        let gas_used = unwrap_opt(self.gas_used, "gas_used")?.parse::<i32>()?;
+
+        Ok(TVMRunMethodSuccess {
+            vm_log,
+            vm_exit_code,
+            stack_boc_b64,
+            gas_used,
+            raw_response: self.raw_response,
+        })
+    }
+}
+
+impl TVMRunMethodSuccess {
+    pub fn stack_parsed(&self) -> Result<TVMStack, TonlibError> { TVMStack::from_boc_b64(&self.stack_boc_b64) }
+
+    pub fn stack_boc(&self) -> Result<Vec<u8>, TonlibError> {
+        Ok(BASE64_STANDARD.decode(self.stack_boc_b64.as_bytes())?)
+    }
+
+    pub fn exit_success(&self) -> bool { self.vm_exit_code == 0 || self.vm_exit_code == 1 }
+}
+
+#[derive(Debug)]
 pub struct TVMSendMsgSuccess {
     pub new_code_boc_b64: Option<String>,
     pub new_data_boc_b64: Option<String>,
@@ -70,75 +139,6 @@ impl TVMSendMsgResponse {
             raw_response: self.raw_response,
         })
     }
-}
-
-#[derive(Debug)]
-pub struct TVMRunMethodSuccess {
-    pub vm_exit_code: i32,
-    pub vm_log: Option<String>,
-    pub stack_boc_b64: String,
-    pub gas_used: i32,
-    pub raw_response: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub struct TVMRunMethodResponse {
-    pub success: bool,
-    pub vm_exit_code: Option<i32>,
-    pub vm_log: Option<String>,
-    pub stack: Option<String>,
-    pub gas_used: Option<String>,
-    pub missing_library: Option<String>,
-    pub error: Option<String>,
-    #[serde(skip)]
-    pub raw_response: String,
-}
-
-impl TVMRunMethodResponse {
-    pub fn from_json(json: String) -> Result<Self, TonlibError> {
-        let mut value: Self = serde_json::from_str(&json)?;
-        value.raw_response = json;
-        Ok(value)
-    }
-
-    pub fn into_success(self) -> Result<TVMRunMethodSuccess, TonlibError> {
-        if !self.success {
-            return Err(TonlibError::TVMRunMethodError {
-                vm_exit_code: self.vm_exit_code,
-                response_raw: self.raw_response,
-            });
-        }
-        let vm_exit_code = unwrap_opt(self.vm_exit_code, "vm_exit_code")?;
-        if vm_exit_code != 0 && vm_exit_code != 1 {
-            return Err(TonlibError::TVMRunMethodError {
-                vm_exit_code: self.vm_exit_code,
-                response_raw: self.raw_response,
-            });
-        }
-
-        let vm_log = self.vm_log;
-        let stack_boc_b64 = unwrap_opt(self.stack, "stack")?;
-        let gas_used = unwrap_opt(self.gas_used, "gas_used")?.parse::<i32>()?;
-
-        Ok(TVMRunMethodSuccess {
-            vm_log,
-            vm_exit_code,
-            stack_boc_b64,
-            gas_used,
-            raw_response: self.raw_response,
-        })
-    }
-}
-
-impl TVMRunMethodSuccess {
-    pub fn stack_parsed(&self) -> Result<TVMStack, TonlibError> { TVMStack::from_boc_b64(&self.stack_boc_b64) }
-
-    pub fn stack_boc(&self) -> Result<Vec<u8>, TonlibError> {
-        Ok(BASE64_STANDARD.decode(self.raw_response.as_bytes())?)
-    }
-
-    pub fn exit_success(&self) -> bool { self.vm_exit_code == 0 || self.vm_exit_code == 1 }
 }
 
 fn unwrap_opt<T>(val: Option<T>, field: &'static str) -> Result<T, TonlibError> {
