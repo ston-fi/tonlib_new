eb1b94052da6b002819d63d18f75c5d96fd2ec9a
Sild <silddev@icloud.com>
2025-06-09 13:03:14 +0200

    fix MintlessAirdropData format

--- ton_lib/src/types/tlb/tep_0074/mintless_airdrop.rs
+++ ton_lib/src/types/tlb/tep_0074/mintless_airdrop.rs
@@ -1,21 +1,23 @@
 use crate::types::tlb::adapters::dict_key_adapters::DictKeyAdapterAddress;
 use crate::types::tlb::adapters::dict_val_adapters::DictValAdapterTLB;
+use crate::types::tlb::adapters::ConstLen;
 use crate::types::tlb::adapters::Dict;
+use crate::types::tlb::block_tlb::coins::Coins;
 use crate::types::tlb::block_tlb::msg_address::MsgAddressInt;
-use crate::types::tlb::block_tlb::var_len::VarLenBytes;
-use num_bigint::BigUint;
 use std::collections::HashMap;
 use ton_lib_macros::TLBDerive;
 
 #[derive(Clone, Debug, TLBDerive)]
 pub struct MintlessAirdropDict {
-    #[tlb_derive(adapter = "Dict::<DictKeyAdapterAddress, DictValAdapterTLB, _, _>::new(256)")]
+    #[tlb_derive(adapter = "Dict::<DictKeyAdapterAddress, DictValAdapterTLB, _, _>::new(267)")]
     pub data: HashMap<MsgAddressInt, MintlessAirdropData>,
 }
 
 #[derive(Clone, Debug, PartialEq, TLBDerive)]
 pub struct MintlessAirdropData {
-    pub amount: VarLenBytes<BigUint, 5>,
+    pub amount: Coins,
+    #[tlb_derive(bits_len = 48)]
     pub start_from: u64,
+    #[tlb_derive(bits_len = 48)]
     pub expired_at: u64,
 }
