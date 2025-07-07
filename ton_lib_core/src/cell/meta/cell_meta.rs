a106571376d848ac97a8de760e0e58f992fe4980
Sild <silddev@pm.me>
2025-07-07 23:52:22 +0200

    calc hash using queue

--- ton_lib_core/src/cell/meta/cell_meta.rs
+++ ton_lib_core/src/cell/meta/cell_meta.rs
@@ -5,6 +5,7 @@ use crate::cell::ton_hash::TonHash;
 use crate::error::TLCoreError;
 use once_cell;
 use once_cell::sync::OnceCell;
+use std::collections::VecDeque;
 
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) struct CellMeta {
@@ -37,7 +38,32 @@ impl CellMeta {
     pub(crate) fn level_mask(&self, cell: &TonCell) -> LevelMask {
         match &self.level_mask {
             LazyLevelMask::Static(mask) => *mask,
-            LazyLevelMask::Dynamic(once) => *once.get_or_init(|| CellMetaBuilder::new(cell).calc_level_mask()),
+            LazyLevelMask::Dynamic(once) => {
+                if let Some(lm) = once.get() {
+                    return *lm;
+                }
+                *once.get_or_init(|| {
+                    let mut queue = VecDeque::with_capacity(cell.refs.len());
+                    for cell_ref in cell.refs.iter().filter(|x| x.meta.level_initialized()) {
+                        if !cell_ref.meta.level_initialized() {
+                            queue.push_back((cell_ref, 0));
+                        }
+                    }
+                    while !queue.is_empty() {
+                        let (cur_cell, cur_ref) = queue.pop_front().unwrap();
+                        if let Some(child) = cur_cell.refs.get(cur_ref) {
+                            queue.push_front((cur_cell, cur_ref + 1));
+                            if !child.meta.level_initialized() {
+                                queue.push_front((child, 0));
+                            }
+                        } else {
+                            let _ = cur_cell.level_mask();
+                        }
+                    }
+
+                    CellMetaBuilder::new(cell).calc_level_mask()
+                })
+            }
         }
     }
 
@@ -51,15 +77,49 @@ impl CellMeta {
     }
 
     fn get_hashes_depths(&self, cell: &TonCell) -> Result<(&[TonHash], &[u16]), TLCoreError> {
-        let level_mask = self.level_mask(cell);
-
         let data = match &self.hashes_depths {
             LazyHashesDepths::Static(hashes_depths) => return Ok((&hashes_depths.0, &hashes_depths.1)),
             LazyHashesDepths::Dynamic(once) => {
-                once.get_or_try_init(|| CellMetaBuilder::new(cell).calc_hashes_and_depths(level_mask))
+                if let Some(data) = once.get() {
+                    return Ok((data.0.as_slice(), data.1.as_slice()));
+                }
+
+                once.get_or_try_init(|| {
+                    let level_mask = self.level_mask(cell);
+                    let mut queue = VecDeque::with_capacity(cell.refs.len());
+                    for cell_ref in cell.refs.iter().filter(|x| x.meta.hash_initialized()) {
+                        queue.push_back((cell_ref, 0));
+                    }
+                    while !queue.is_empty() {
+                        let (cur_cell, cur_ref) = queue.pop_front().unwrap();
+                        if let Some(child) = cur_cell.refs.get(cur_ref) {
+                            queue.push_front((cur_cell, cur_ref + 1));
+                            if !child.meta.hash_initialized() {
+                                queue.push_front((child, 0));
+                            }
+                        } else {
+                            let _ = cur_cell.hash()?;
+                        }
+                    }
+                    CellMetaBuilder::new(cell).calc_hashes_and_depths(level_mask)
+                })
             }
         };
         data.map(|(hashes, depths)| (hashes.as_slice(), depths.as_slice()))
+    }
+
+    fn level_initialized(&self) -> bool {
+        match &self.hashes_depths {
+            LazyHashesDepths::Static(_) => true,
+            LazyHashesDepths::Dynamic(once) => once.get().is_some(),
+        }
+    }
+
+    fn hash_initialized(&self) -> bool {
+        match &self.level_mask {
+            LazyLevelMask::Static(_) => true,
+            LazyLevelMask::Dynamic(once) => once.get().is_some(),
+        }
     }
 }
 
